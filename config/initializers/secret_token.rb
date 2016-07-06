e3a84901903fe03c6186b44f364359feaa3b971f
brapastor <brapastor@gmail.com>
2016-07-06 00:10:20 -0500

    rails cero

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiBlog::Application.config.secret_key_base = '75fb39b1b10e3313a4ba73f6f99f960470a5c0a7479d373e83e02e0283310709a9417b2dd6d9ba5e93fc82d4f468a1fc0b2e7e057f67507e41287a2abd756aac'
