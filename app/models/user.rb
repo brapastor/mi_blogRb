758af49b5466b541603471514a5d59ee1b140dfe
brapastor <brapastor@gmail.com>
2016-07-11 00:14:35 -0500

    agregado de crud y validacion

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  #Validando datos
+
+  validates :nombre, presence: true, length: { maximum: 5 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true , format: {with: VALID_EMAIL_REGEX}
+
+end
